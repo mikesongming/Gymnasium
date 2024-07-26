e8e3cb1aa649c9d813004b456fdea2cf3e5db72d
Mike Song <mikesongming@gmail.com>
2024-07-26 11:45:33 +0800

    remove MuJocoPyEnv and fixed bugs for MacOS

--- gymnasium/envs/__init__.py
+++ gymnasium/envs/__init__.py
@@ -181,13 +181,6 @@ register(
 # manipulation
 
 register(
-    id="Reacher-v2",
-    entry_point="gymnasium.envs.mujoco.reacher:ReacherEnv",
-    max_episode_steps=50,
-    reward_threshold=-3.75,
-)
-
-register(
     id="Reacher-v4",
     entry_point="gymnasium.envs.mujoco.reacher_v4:ReacherEnv",
     max_episode_steps=50,
@@ -199,13 +192,6 @@ register(
     entry_point="gymnasium.envs.mujoco.reacher_v5:ReacherEnv",
     max_episode_steps=50,
     reward_threshold=-3.75,
-)
-
-register(
-    id="Pusher-v2",
-    entry_point="gymnasium.envs.mujoco.pusher:PusherEnv",
-    max_episode_steps=100,
-    reward_threshold=0.0,
 )
 
 register(
@@ -225,13 +211,6 @@ register(
 # balance
 
 register(
-    id="InvertedPendulum-v2",
-    entry_point="gymnasium.envs.mujoco.inverted_pendulum:InvertedPendulumEnv",
-    max_episode_steps=1000,
-    reward_threshold=950.0,
-)
-
-register(
     id="InvertedPendulum-v4",
     entry_point="gymnasium.envs.mujoco.inverted_pendulum_v4:InvertedPendulumEnv",
     max_episode_steps=1000,
@@ -243,13 +222,6 @@ register(
     entry_point="gymnasium.envs.mujoco.inverted_pendulum_v5:InvertedPendulumEnv",
     max_episode_steps=1000,
     reward_threshold=950.0,
-)
-
-register(
-    id="InvertedDoublePendulum-v2",
-    entry_point="gymnasium.envs.mujoco.inverted_double_pendulum:InvertedDoublePendulumEnv",
-    max_episode_steps=1000,
-    reward_threshold=9100.0,
 )
 
 register(
@@ -269,20 +241,6 @@ register(
 # runners
 
 register(
-    id="HalfCheetah-v2",
-    entry_point="gymnasium.envs.mujoco.half_cheetah:HalfCheetahEnv",
-    max_episode_steps=1000,
-    reward_threshold=4800.0,
-)
-
-register(
-    id="HalfCheetah-v3",
-    entry_point="gymnasium.envs.mujoco.half_cheetah_v3:HalfCheetahEnv",
-    max_episode_steps=1000,
-    reward_threshold=4800.0,
-)
-
-register(
     id="HalfCheetah-v4",
     entry_point="gymnasium.envs.mujoco.half_cheetah_v4:HalfCheetahEnv",
     max_episode_steps=1000,
@@ -294,20 +252,6 @@ register(
     entry_point="gymnasium.envs.mujoco.half_cheetah_v5:HalfCheetahEnv",
     max_episode_steps=1000,
     reward_threshold=4800.0,
-)
-
-register(
-    id="Hopper-v2",
-    entry_point="gymnasium.envs.mujoco.hopper:HopperEnv",
-    max_episode_steps=1000,
-    reward_threshold=3800.0,
-)
-
-register(
-    id="Hopper-v3",
-    entry_point="gymnasium.envs.mujoco.hopper_v3:HopperEnv",
-    max_episode_steps=1000,
-    reward_threshold=3800.0,
 )
 
 register(
@@ -325,20 +269,6 @@ register(
 )
 
 register(
-    id="Swimmer-v2",
-    entry_point="gymnasium.envs.mujoco.swimmer:SwimmerEnv",
-    max_episode_steps=1000,
-    reward_threshold=360.0,
-)
-
-register(
-    id="Swimmer-v3",
-    entry_point="gymnasium.envs.mujoco.swimmer_v3:SwimmerEnv",
-    max_episode_steps=1000,
-    reward_threshold=360.0,
-)
-
-register(
     id="Swimmer-v4",
     entry_point="gymnasium.envs.mujoco.swimmer_v4:SwimmerEnv",
     max_episode_steps=1000,
@@ -353,18 +283,6 @@ register(
 )
 
 register(
-    id="Walker2d-v2",
-    max_episode_steps=1000,
-    entry_point="gymnasium.envs.mujoco.walker2d:Walker2dEnv",
-)
-
-register(
-    id="Walker2d-v3",
-    max_episode_steps=1000,
-    entry_point="gymnasium.envs.mujoco.walker2d_v3:Walker2dEnv",
-)
-
-register(
     id="Walker2d-v4",
     max_episode_steps=1000,
     entry_point="gymnasium.envs.mujoco.walker2d_v4:Walker2dEnv",
@@ -374,20 +292,6 @@ register(
     id="Walker2d-v5",
     max_episode_steps=1000,
     entry_point="gymnasium.envs.mujoco.walker2d_v5:Walker2dEnv",
-)
-
-register(
-    id="Ant-v2",
-    entry_point="gymnasium.envs.mujoco.ant:AntEnv",
-    max_episode_steps=1000,
-    reward_threshold=6000.0,
-)
-
-register(
-    id="Ant-v3",
-    entry_point="gymnasium.envs.mujoco.ant_v3:AntEnv",
-    max_episode_steps=1000,
-    reward_threshold=6000.0,
 )
 
 register(
@@ -405,18 +309,6 @@ register(
 )
 
 register(
-    id="Humanoid-v2",
-    entry_point="gymnasium.envs.mujoco.humanoid:HumanoidEnv",
-    max_episode_steps=1000,
-)
-
-register(
-    id="Humanoid-v3",
-    entry_point="gymnasium.envs.mujoco.humanoid_v3:HumanoidEnv",
-    max_episode_steps=1000,
-)
-
-register(
     id="Humanoid-v4",
     entry_point="gymnasium.envs.mujoco.humanoid_v4:HumanoidEnv",
     max_episode_steps=1000,
@@ -425,12 +317,6 @@ register(
 register(
     id="Humanoid-v5",
     entry_point="gymnasium.envs.mujoco.humanoid_v5:HumanoidEnv",
-    max_episode_steps=1000,
-)
-
-register(
-    id="HumanoidStandup-v2",
-    entry_point="gymnasium.envs.mujoco.humanoidstandup:HumanoidStandupEnv",
     max_episode_steps=1000,
 )
 
